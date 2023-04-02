2c140b6138e38b2d56361b3d9f3c8bef0b5e4414
Ritvik Nag <nagrim7@gmail.com>
2023-04-02 19:04:03 -0400

    add publish to pypi manual script

--- scripts/publish_to_pypi.sh
+++ scripts/publish_to_pypi.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
 # Use this script to manually publish to PyPI!
-python3 setup.py sdist bdist_wheel
-python3 -m twine upload dist/*
+
+python setup.py sdist bdist_wheel
+
+python -c 'import pkgutil; exit(0 if pkgutil.find_loader("twine") else 2)' || { echo 'Installing twine...'; pip install --quiet twine; }
+
+while true; do
+    read -p "Do you want to upload to PyPI (Y/N)? " yn
+    case $yn in
+        [Yy]* ) python -m twine upload dist/*; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
